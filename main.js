1eccbb5e2924f42ff0358f5a85443267fed2ea1d
BorisVelcoro <boris94z@hotmail.com>
2022-09-18 17:01:42 +0300

    feat: add player1 and player2 objects

---
+++ main.js
@@ -0,0 +1,19 @@
+let player1 = {
+  name: 'Scorpion',
+  hp: 100,
+  img: 'https://www.fightersgeneration.com/characters3/scorpion-mk3-fix2.gif',
+  weapon: ['Hook', 'Fists', 'BFG'],
+  attack: function() {
+    console.log(`${this.name} Fight...`);
+  }
+}
+
+let player2 = {
+  name: 'Subzero',
+  hp: 100,
+  img: 'https://www.fightersgeneration.com/characters3/scorpion-mk3-fix2.gif',
+  weapon: ['Ice', 'Fists', 'AK47'],
+  attack: function() {
+    console.log(`${this.name} Fight...`);
+  }
+}
