006c5c868790053fe6588196094281eee825a4ea
BorisVelcoro <boris94z@hotmail.com>
2022-09-18 17:45:57 +0300

    feat:add function for create player, fix: img in player2 object

--- main.js
+++ main.js
@@ -11,9 +11,38 @@ let player1 = {
 let player2 = {
   name: 'Subzero',
   hp: 100,
-  img: 'https://www.fightersgeneration.com/characters3/scorpion-mk3-fix2.gif',
+  img: 'https://www.fightersgeneration.com/characters3/subzero-stance.gif',
   weapon: ['Ice', 'Fists', 'AK47'],
   attack: function() {
     console.log(`${this.name} Fight...`);
   }
+}
+
+let createPlayer = function() {
+  const $root = document.querySelector('.root');
+
+  const $player1 = document.createElement('div');
+  $player1.classList.add('player1');
+  
+  const $progressbar = document.createElement('div');
+  $progressbar.classList.add('progressbar');
+
+  const $character = document.createElement('div');
+  $character.classList.add('character');
+
+  const $life = document.createElement('div');
+  $life.classList.add('life');
+
+  const $name = document.createElement('div');
+  $name.classList.add('name');
+
+  const $img = document.createElement('img');
+  $img.src = player1.img;
+  
+  $root.appendChild($player1);
+  $player1.appendChild($progressbar);
+  $player1.appendChild($character);
+  $progressbar.appendChild($life);
+  $progressbar.appendChild($name);
+  $character.appendChild($img);
 }
